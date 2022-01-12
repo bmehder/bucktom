0ceaaf3fbcd958d4c3a3e93851fba1742cab3994
bmehder <bmehder@gmail.com>
2022-01-12 14:47:50 +0100

    moved on click and transition on Poster to parent element

--- src/lib/Poster.svelte
+++ src/lib/Poster.svelte
@@ -3,33 +3,26 @@
   import { sineInOut } from 'svelte/easing'
 </script>
 
-<div on:click>
-  <aside>
-    <svg
-      class="w-6 h-6"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-      /><path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      /></svg
-    >
-    <img
-      src="video.jpg"
-      alt="Bucktom Video Poster"
-      on:click
-      out:fade={{ delay: 0, duration: 300, easing: sineInOut }}
-    />
-  </aside>
+<div on:click out:fade={{ delay: 0, duration: 300, easing: sineInOut }}>
+  <svg
+    class="w-6 h-6"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    ><path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+    /><path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    /></svg
+  >
+  <img src="video.jpg" alt="Bucktom Video Poster" />
 </div>
 
 <style>
