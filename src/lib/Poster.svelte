eacebc70602d1a9682fb9fed783fd435ad4358be
bmehder <bmehder@gmail.com>
2022-01-12 14:08:04 +0100

    forgot to save Poster

--- src/lib/Poster.svelte
+++ src/lib/Poster.svelte
@@ -1,5 +1,6 @@
 <script>
   import { fade } from 'svelte/transition'
+  import { sineInOut } from 'svelte/easing'
 </script>
 
 <div>
@@ -7,7 +8,7 @@
     src="https://img.youtube.com/vi/Ub_XSLg-eO8/0.jpg"
     alt="Bucktom Video Poster"
     on:click
-    out:fade={{ delay: 0, duration: 300 }}
+    out:fade={{ delay: 0, duration: 300, easing: sineInOut }}
   />
 </div>
 
